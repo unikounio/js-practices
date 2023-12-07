fedb17d112a16364ae4e51a39e519e0d9cbfb9f1
unikounio <bubbbble428@gmail.com>
2023-12-07 18:07:23 +0900

    文字列結合を用いていた箇所をテンプレート文字列に置き換えた

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -6,7 +6,7 @@ const today = new Date();
 const year = argv.y || today.getFullYear();
 const month = argv.m || today.getMonth() + 1;
 
-console.log("      " + month + "月 " + year + "        ");
+console.log(`      ${month}月 ${year}        `);
 console.log("日 月 火 水 木 金 土");
 
 const dt = DateTime.local(year, month);
