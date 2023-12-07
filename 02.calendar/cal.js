fbe8b786b85be7b7032285d09b94b0beec2770cf
unikounio <bubbbble428@gmail.com>
2023-12-07 20:43:18 +0900

    -mと-yの値が有効値でない場合にエラー文を表示するようにした

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,8 +3,21 @@ import { DateTime } from "luxon";
 
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-const year = argv.y || today.getFullYear();
-const month = argv.m || today.getMonth() + 1;
+const year = argv.y === undefined ? today.getFullYear() : Number(argv.y);
+const month = argv.m === undefined ? today.getMonth() + 1 : Number(argv.m);
+
+if (!Number.isInteger(year)) {
+  console.error(`cal: not a valid year ${year}`);
+  process.exit(1);
+} else if (year < 1 || year > 9999) {
+  console.error(`cal: year \`${year}' not in range 1..9999`);
+  process.exit(1);
+}
+
+if (month < 1 || month > 12 || !Number.isInteger(month)) {
+  console.error(`cal: ${month} is neither a month number (1..12) nor a name`);
+  process.exit(1);
+}
 
 console.log(`      ${month}月 ${year}        `);
 console.log("日 月 火 水 木 金 土");
