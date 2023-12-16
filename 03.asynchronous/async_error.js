9880eda661d991d1fb70c15cf16ff6359a25425e
unikounio <bubbbble428@gmail.com>
2023-12-17 02:21:22 +0900

    Top-level awaitを使用してリファクタリング

--- 03.asynchronous/async_error.js
+++ 03.asynchronous/async_error.js
@@ -5,23 +5,19 @@ import {
   closePromise,
 } from "./sqlite_promisification.js";
 
-const asyncErrorHandling = async () => {
-  await runPromise(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-  );
-  try {
-    await runPromise(db, "INSERT INTO books(title) VALUES(?)", null);
-  } catch (err) {
-    console.error(err.message);
-  }
-  try {
-    await getPromise(db, "SELECT name FROM books");
-  } catch (err) {
-    console.error(err.message);
-  }
-  await runPromise(db, "DROP TABLE books");
-  closePromise(db);
-};
-
-asyncErrorHandling();
+await runPromise(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+);
+try {
+  await runPromise(db, "INSERT INTO books(title) VALUES(?)", null);
+} catch (err) {
+  console.error(err.message);
+}
+try {
+  await getPromise(db, "SELECT name FROM books");
+} catch (err) {
+  console.error(err.message);
+}
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
--- 03.asynchronous/async_success.js
+++ 03.asynchronous/async_success.js
@@ -5,21 +5,17 @@ import {
   closePromise,
 } from "./sqlite_promisification.js";
 
-const asyncSuccess = async () => {
-  await runPromise(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-  );
-  const insertedBook = await runPromise(
-    db,
-    "INSERT INTO books(title) VALUES(?)",
-    "JSふりがなプログラミング"
-  );
-  console.log(`id: ${insertedBook.lastID}`);
-  const book = await getPromise(db, "SELECT * FROM books");
-  console.log(`id: ${book.id}, title: ${book.title}`);
-  await runPromise(db, "DROP TABLE books");
-  closePromise(db);
-};
-
-asyncSuccess();
+await runPromise(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+);
+const insertedBook = await runPromise(
+  db,
+  "INSERT INTO books(title) VALUES(?)",
+  "JSふりがなプログラミング"
+);
+console.log(`id: ${insertedBook.lastID}`);
+const book = await getPromise(db, "SELECT * FROM books");
+console.log(`id: ${book.id}, title: ${book.title}`);
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
