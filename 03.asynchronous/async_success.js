0e38617d96e2e6d07277ed999ca7d5828efca45b
unikounio <bubbbble428@gmail.com>
2023-12-21 16:12:35 +0900

    変数名が修正漏れにより統一されていなかった点に対応

--- 03.asynchronous/async_success.js
+++ 03.asynchronous/async_success.js
@@ -9,12 +9,12 @@ await runPromise(
   db,
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
 );
-const insertedBook = await runPromise(
+const result = await runPromise(
   db,
   "INSERT INTO books(title) VALUES(?)",
   "JSふりがなプログラミング"
 );
-console.log(`id: ${insertedBook.lastID}`);
+console.log(`id: ${result.lastID}`);
 const book = await getPromise(db, "SELECT * FROM books");
 console.log(`id: ${book.id}, title: ${book.title}`);
 await runPromise(db, "DROP TABLE books");
